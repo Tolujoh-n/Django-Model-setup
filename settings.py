c3031261d4ea005ba913e2bdad699881dbfc1783
olagbile john <olagbilejohn@gmail.com>
2022-10-15 02:04:51 +0100

    Django model

---
+++ settings.py
@@ -0,0 +1,19 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '@OLagBile2',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
+
+
